b05eb776f243133d2bf5ec4aa98c015c83307749
Sandarsh Naroju <narojusandarsh18@gmail.com>
2023-09-13 15:28:17 +0530

    fixed image component

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,30 +1,20 @@
 import React, {useEffect} from 'react';
-import {Button} from 'react-native-paper';
 import {TouchableOpacity, Image as PaperImage} from 'react-native';
 
 function Image({elementProps, getViewItems, onElementLoaded}) {
   useEffect(() => {
     onElementLoaded(elementProps);
   }, []);
-  const imgSource =
-    elementProps != null && elementProps['value'] != null
-      ? elementProps['value'].indexOf('http') == 0
-        ? {uri: elementProps['value']}
-        : elementProps['value']
-      : null;
-  if (imgSource) {
-    return (
-      <TouchableOpacity {...elementProps}>
-        <PaperImage
-          {...elementProps['props']}
-          {...elementProps}
-          source={imgSource}
-        />
-      </TouchableOpacity>
-    );
-  } else {
-    return null;
-  }
+
+  return (
+    <TouchableOpacity {...elementProps}>
+      <PaperImage
+        {...elementProps['props']}
+        {...elementProps}
+        source={elementProps['value']}
+      />
+    </TouchableOpacity>
+  );
 }
 
 export default Image;
