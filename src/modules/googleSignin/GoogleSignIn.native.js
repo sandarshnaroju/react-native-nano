12607eac384468f3ee6ed725c7f648583e81dde4
Sandarsh Naroju <narojusandarsh18@gmail.com>
2023-10-02 21:50:02 +0530

    google signin removed

--- src/modules/googleSignin/GoogleSignIn.native.js
+++ src/modules/googleSignin/GoogleSignIn.native.js
@@ -1,7 +1,10 @@
-import {
-  GoogleSignin,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
+// import {
+//   GoogleSignin,
+//   statusCodes,
+// } from '@react-native-google-signin/google-signin';
 
-const CustomGoogleSignIn = {GoogleSignin, statusCodes};
+// const CustomGoogleSignIn = {GoogleSignin, statusCodes};
+// export default CustomGoogleSignIn;
+
+const CustomGoogleSignIn = {};
 export default CustomGoogleSignIn;
