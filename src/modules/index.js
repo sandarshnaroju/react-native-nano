02458c7a30dc64175471de28d9014bb6c17f4e47
Sandarsh Naroju <narojusandarsh18@gmail.com>
2023-10-06 23:58:41 +0530

    added more stuff to moduleParams

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,7 +3,29 @@ import getNotification from './notifications/Notifications';
 
 import getPermissionInstance from './permissions/Permissions';
 import getSession from './session/Session';
-import {Dimensions} from 'react-native';
+import {
+  Dimensions,
+  Alert,
+  Linking,
+  Share,
+  Keyboard,
+  Appearance,
+  AppState,
+  InteractionManager,
+  PanResponder,
+  Platform,
+  PixelRatio,
+  BackHandler,
+  LayoutAnimation,
+  UIManager,
+  DevSettings,
+  Animated,
+  Easing,
+  Systrace,
+  Vibration,
+  Settings,
+  AccessibilityInfo,
+} from 'react-native';
 // import CustomGoogleSignIn from './googleSignin/GoogleSignIn';
 import ImagePicker from './imagepicker/ImagePicker';
 import DeviceInfo from './deviceinfo/DeviceInfo';
@@ -30,10 +52,31 @@ const getModuleParams = ({callBack}) => {
     notifications,
     permissions,
     session,
-    deviceDimensions,
+    dimensions: deviceDimensions,
     // ...CustomGoogleSignIn,
 
     deviceInfo: DeviceInfo,
+    alert: Alert,
+    linking: Linking,
+    share: Share,
+    keyboard: Keyboard,
+    appearance: Appearance,
+    appState: AppState,
+    interactionManager: InteractionManager,
+    paneResponder: PanResponder,
+    platform: Platform,
+    pixelRatio: PixelRatio,
+    backHandler: BackHandler,
+    layoutAnimation: LayoutAnimation,
+    uiManager: UIManager,
+    devSettings: DevSettings,
+    animated: Animated,
+    easing: Easing,
+    systrace: Systrace,
+
+    vibration: Vibration,
+    settings: Settings,
+    accessibilityInfo: AccessibilityInfo,
   };
   if (ImagePicker) {
     moduleParameters['imagePicker'] = ImagePicker;
