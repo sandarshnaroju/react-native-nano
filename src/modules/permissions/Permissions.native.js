2e96aafae88ad8fd80ca61a6e8d6aec7427fdb20
Sandarsh Naroju <narojusandarsh18@gmail.com>
2023-10-06 23:42:18 +0530

    simplified permissions

--- src/modules/permissions/Permissions.native.js
+++ src/modules/permissions/Permissions.native.js
@@ -1,58 +1,6 @@
-import {requestMultiple, request} from 'react-native-permissions';
-class Permissions {
-  constructor(props) {}
-  requestPermissions(permissionsArray) {
-    return requestMultiple(permissionsArray);
-  }
-  requestAPermission(permission) {
-    return request(permission);
-  }
-  checkPermissionGrantStatus(permission) {
-    return request(permission)
-      .then(res => {
-        if (res != null) {
-          if (res === 'granted') {
-            return new Promise.resolve(true);
-          } else {
-            return new Promise.resolve(false);
-          }
-        }
-      })
-      .catch(() => {
-        return new Promise.resolve(false);
-      });
-  }
-  checkIfPermissionsGranted(permissionsArray) {
-    return requestMultiple(permissionsArray)
-      .then(res => {
-        if (res != null) {
-          const checkIfNotGranted = permissionsArray.find(
-            permission => res[permission] !== 'granted',
-          );
-          if (checkIfNotGranted != null) {
-            return new Promise.resolve(false);
-          } else {
-            return new Promise.resolve(true);
-          }
-        }
-      })
-      .catch(() => {
-        return new Promise.resolve(false);
-      });
-  }
-}
-var perm = null;
+import Permission from 'react-native-permissions';
+
 const getPermissionInstance = () => {
-  try {
-    if (perm == null) {
-      require('react-native-permissions');
-
-      perm = new Permissions();
-    }
-  } catch (e) {
-    perm = null;
-  }
-
-  return perm;
+  return Permission;
 };
 export default getPermissionInstance;
